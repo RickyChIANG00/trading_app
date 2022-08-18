4c4d4d64f6413072ccb3bbe747c775119fdecde6
RickyChIANG00 <yoyo8751200@gmail.com>
2022-08-18 22:02:25 +1000

    "Added prices details and tradingview wadget"

--- main.py
+++ main.py
@@ -13,8 +13,18 @@ def index(request: Request):
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT id, symbol, name FROM stock
+        SELECT id, symbol, name FROM stock ORDER BY symbol
     """)
     rows = cursor.fetchall()
-    
     return templates.TemplateResponse("index.html", {"request": request, "stocks": rows}) 
+
+@app.get('/stock/{symbol}')
+def stock_detail(request: Request, symbol):
+    connection = sqlite3.connect(config.DB_FILE_PATH)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT * FROM stock  WHERE symbol = ? 
+    """, (symbol))
+    rows = cursor.fetchone()
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stocks": rows}) 
