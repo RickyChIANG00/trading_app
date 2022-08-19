54d44dd2240a9348beac5e744560e33be19902f4
RickyChIANG00 <yoyo8751200@gmail.com>
2022-08-19 18:30:06 +1000

    "Added strategy dropdown list in price detail"

--- create_db.py
+++ create_db.py
@@ -27,4 +27,20 @@ cursor.execute("""
     )
 """)
 
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS strategy(
+        id INTEGER PRIMARY KEY,
+        name NOT NULL
+    ) 
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS stock_strategy(
+        stock_id INTEGER NOT NULL,
+        strategy_id INTEGER NOT NULL,
+        FOREIGN KEY (stock_id) REFERENCES stock(id),
+        FOREIGN KEY (strategy_id) REFERENCES strategy(id)
+    ) 
+""")
+
 connection.commit()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from re import template
 import sqlite3, config
-from fastapi import Request
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI() 
@@ -23,6 +23,13 @@ def stock_detail(request: Request, symbol):
     connection = sqlite3.connect(config.DB_FILE_PATH)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT * FROM strategy
+    """)
+
+    strategies = cursor.fetchall()
+
     cursor.execute("""
         SELECT * FROM stock WHERE symbol = ? 
     """, (symbol, ))
@@ -34,4 +41,41 @@ def stock_detail(request: Request, symbol):
     
     prices = cursor.fetchall()
 
-    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock" : row, "prices" : prices}) 
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock" : row, "prices" : prices, "strategies" : strategies}) 
+
+
+@app.post('/apply_strategy')
+def apply_strategy(strategy_id:int = Form(...),stock_id:int = Form(...)):
+    connection = sqlite3.connect(config.DB_FILE_PATH)
+    cursor = connection.cursor()
+    print(strategy_id)
+
+    cursor.execute("""
+        INSERT INTO stock_strategy (stock_id, strategy_id) VALUES (?,?)
+    """, (stock_id, strategy_id, ))
+
+    connection.commit()
+
+    return RedirectResponse(url=f"/strategy/{strategy_id}", status_code=303)
+
+@app.get('/strategy/{strategy_id}')
+def index(request: Request, strategy_id:int):
+    connection = sqlite3.connect(config.DB_FILE_PATH)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, name FROM strategy
+        WHERE id = ?
+    """, (strategy_id, ))
+
+    strategy = cursor.fetchone()
+
+    cursor.execute("""
+        SELECT symbol, name FROM stock
+        JOIN stock_strategy on stock_strategy.stock_id = stock.id
+        WHERE strategy_id = ?
+    """, (strategy_id, ))
+    stocks = cursor.fetchall()
+
+    return templates.TemplateResponse("strategy.html", {"request":request, "stocks":stocks, "strategy" : strategy})
